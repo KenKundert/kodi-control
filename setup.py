3cafac364aa841ecb43bbb6bbd924e6a4d713958
Ken Kundert <ken@theKunderts.net>
2021-08-12 22:12:21 -0700

    tweak setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.rst", encoding="utf-8") as file:
+    readme = file.read()
+
 setup(
     name='kodi-control',
-    version='0.0',
+    version='0.0.0',
     description='interactive tty-based remote control for Kodi',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
     scripts='kodi-control'.split(),
     install_requires='appdirs docopt inform nestedtext shlib voluptuous'.split(),
 )
